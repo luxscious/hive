19132f264a244bc161ca1fd662f64a00fd7ab608
luxscious <57682940+luxscious@users.noreply.github.com>
2022-01-09 01:53:07 -0500

    End points and express server setup
    localhost:5000,
    Home page started

--- client/src/Home.js
+++ client/src/Home.js
@@ -3,28 +3,116 @@ import { getAuth } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCookies } from "react-cookie";
 import NavBar from "./components/NavBar";
+import { makeStyles } from "@material-ui/core";
+import bg from "./assets/combBg.svg";
+import ProjectCard from "./components/ProjectCard";
+import text from "./assets/inspoText.svg";
+import plus from "./assets/plus.svg";
+import { Link } from "react-router-dom";
+const useStyles = makeStyles((theme) => ({
+  container: {
+    position: "fixed",
+    padding: 0,
+    margn: 0,
+    width: "100%",
+    height: "100%",
+    backgroundImage: `url(${bg})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover ",
 
+    display: "flex",
+
+    alignItems: "center",
+    flexDirection: "column",
+    overflow: "hidden",
+  },
+
+  inspoText: {
+    backgroundImage: `url(${text})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    minWidth: "100%",
+    marginTop: 132,
+    minHeight: 191,
+  },
+  items: {
+    width: 1090,
+    backgroundColor: "#C4C4C4",
+    height: "100%",
+    marginTop: 20,
+    borderRadius: "20px 20px 0 0",
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  cards: {
+    marginTop: 61,
+    paddingLeft: 113,
+    overflow: "scroll",
+    alignSelf: "center",
+  },
+  post: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignSelf: "flex-end",
+    // backgroundColor: "red",
+    width: 250,
+    height: 38,
+    paddingTop: 40,
+    paddingRight: 47,
+    "&:hover": {
+      cursor: "pointer",
+    },
+    textDecoration: "none",
+  },
+}));
 const Home = () => {
   const auth = getAuth();
 
   const [user, loading, error] = useAuthState(auth);
   const [username, setUsername] = useState("");
-
+  const classes = useStyles();
   const [cookies, setCookie, removeCookie] = useCookies(["user"]);
   const signOut = (e) => {
     e.preventDefault();
     removeCookie("oAuth");
     auth.signOut();
   };
+
   return (
-    <div>
+    <>
       <NavBar page={"community"} />
-      {user ? (
-        <form onSubmit={signOut}>
-          <button type="submit">Sign Out</button>
-        </form>
-      ) : null}
-    </div>
+      <div className={classes.container}>
+        <div className={classes.inspoText}></div>
+        <div className={classes.items}>
+          {user ? (
+            <Link to="/" className={classes.post}>
+              <h1
+                style={{
+                  fontSize: 28,
+                  fontFamily: "Raleway",
+                  color: "#656565",
+                  alignSelf: "center",
+                  paddingRight: 8,
+                }}
+              >
+                POST PROJECT{" "}
+              </h1>
+              <img src={plus} alt="plus btn" width="38"></img>
+            </Link>
+          ) : null}
+          <div className={classes.cards}>
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 export default Home;
---
+++ server.js
@@ -0,0 +1,56 @@
+let projects = require("./data.json");
+const express = require("express"); //Line 1
+const app = express(); //Line 2
+const port = process.env.PORT || 5000; //Line 3
+
+// This displays message that the server running and listening to specified port
+app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+
+//get project details given username and name of project
+app.get("/projectDetails", (req, res) => {
+  let username = req.query.username;
+  let project = req.query.project;
+  const allProjects = [];
+  for (const i in projects) {
+    if (
+      projects[i].username === username &&
+      projects[i].projectName === project
+    ) {
+      allProjects.push(projects[i]);
+    }
+  }
+  res.json(allProjects);
+});
+
+//get all projects for a given user
+app.get("/userProjects", (req, res) => {
+  let username = req.query.username;
+  const allProjects = [];
+  for (const i in projects) {
+    if (projects[i].username === username) {
+      allProjects.push(projects[i]);
+    }
+  }
+  res.json(allProjects);
+});
+
+//get all projects
+app.get("/projects", (req, res) => {
+  const allProjects = [];
+  for (const i in projects) {
+    allProjects.push(projects[i]);
+  }
+  res.json(allProjects);
+});
+
+//get all projects that are a specifc category
+app.get("/categoryProjects", (req, res) => {
+  const category = req.query.category;
+  const allProjects = [];
+  for (const i in projects) {
+    if (projects[i].categories.includes(category)) {
+      allProjects.push(projects[i]);
+    }
+  }
+  res.json(allProjects);
+});
