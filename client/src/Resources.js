4d2a10cc68f01856759d94e504d759bc7b737a38
luxscious <57682940+luxscious@users.noreply.github.com>
2022-01-09 06:41:38 -0500

    Resource Page

--- client/src/Resources.js
+++ client/src/Resources.js
@@ -1,276 +1,192 @@
-import React, { useContext, useEffect, useState } from "react";
-import { getAuth } from "firebase/auth";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useCookies } from "react-cookie";
+import React from "react";
 import NavBar from "./components/NavBar";
 import { makeStyles } from "@material-ui/core";
-import bg from "./assets/combBg.svg";
-import ProjectCard from "./components/ProjectCard";
-import text from "./assets/inspoText.svg";
-import plus from "./assets/plus.svg";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import { StylesContext } from "@material-ui/styles";
+import text1 from "./assets/resource1.svg";
+import git from "./assets/githubIcon.svg";
+import text2 from "./assets/resource2.svg";
+import text3 from "./assets/resource3.svg";
+import bee from "./assets/collaborate.svg";
+import eye from "./assets/eye.svg";
+import add from "./assets/add.svg";
+import star from "./assets/Star.svg";
 const useStyles = makeStyles((theme) => ({
   container: {
-    position: "fixed",
-    padding: 0,
-    margn: 0,
+    display: "flex",
+    flexDirection: "column",
     width: "100%",
-    height: "100%",
-    backgroundImage: `url(${bg})`,
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover ",
-
-    display: "flex",
-    overflow: "auto",
-    alignItems: "center",
-    flexDirection: "column",
+    overflow: "scroll",
+    marginTop: 90,
   },
 
-  inspoText: {
-    backgroundImage: `url(${text})`,
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    minWidth: "100%",
-    marginTop: 132,
-    minHeight: 191,
-  },
-  items: {
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    width: 1090,
-    backgroundColor: "#E5E5E5",
-    minHeight: "100%",
-    marginTop: 20,
-    borderRadius: "20px 20px 0 0",
-    alignItems: "center",
-  },
-  cards: {
-    height: "100%",
-    minWidth: 760,
-    paddingTop: 61,
-    display: "flex",
-    flexDirection: "column",
-  },
-  post: {
+  section: {
+    backgroundColor: "#FF998A",
+    paddingTop: 50,
+    paddingBottom: 50,
+
     display: "flex",
     flexDirection: "row",
-    justifyContent: "flex-end",
-    alignSelf: "flex-end",
-    width: 250,
-    height: 38,
-    paddingTop: 40,
-    paddingRight: 47,
-    "&:hover": {
-      cursor: "pointer",
-    },
-    textDecoration: "none",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  sideBar: {
+  section2: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  section3: {
+    display: "flex",
     flexDirection: "column",
-    width: 216,
-    borderColor: "#E5E5E5",
-    bordWidth: 3,
-    border: "solid",
-    borderRadius: 20,
-    maxHeight: 640,
-    backgroundColor: "white",
-    marginTop: 20,
-    marginRight: 30,
-  },
-  button: {
-    width: 132,
-    backgroundColor: "white",
-    border: "none",
-    textAlign: "left",
-    padding: 14,
+    justifyContent: "center",
+    alignSelf: "center",
+    width: 1200,
+    paddingLeft: 90,
     fontFamily: "Raleway",
-    fontWeight: "bold",
-    fontSize: 18,
-    "&:hover": {
-      cursor: "pointer",
-    },
+    fontWeight: "Bold",
   },
-  sectionOne: {
+  section4: {
     display: "flex",
     flexDirection: "column",
-    padding: 20,
-    borderBottom: "solid #E5E5E5",
+    justifyContent: "center",
+    alignSelf: "center",
+    paddingTop: 80,
+    width: 1300,
+    fontFamily: "Raleway",
+    fontWeight: "Bold",
   },
-  sectionTwo: {
-    display: "flex",
-    flexDirection: "column",
-    padding: 20,
+  logo: {
+    width: 115,
+    height: 115,
+  },
+  logoText: {
+    fontFamily: "Raleway Light",
+    fontSize: 25,
+    textAlign: "center",
   },
 }));
 
-function RenderList(list) {
-  return list.map((x) => {
-    return <ProjectCard project={x} />;
-  });
-}
-
 const Resources = () => {
-  const auth = getAuth();
-  const [user, loading, error] = useAuthState(auth);
-  const [username, setUsername] = useState("");
   const classes = useStyles();
-  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
-  const [listState, setListState] = useState("");
-  const [projects, setProjects] = useState([]);
-
-  const signOut = (e) => {
-    e.preventDefault();
-    removeCookie("oAuth");
-    auth.signOut();
-  };
-  //Get all lists and store them
-  useEffect(() => {
-    let query;
-    if (listState === "") {
-      query = "http://localhost:5000/projects";
-    } else if (listState === "mentor") {
-      query = "http://localhost:5000/needMentors";
-    } else {
-      query = "http://localhost:5000/categoryProjects?category=" + listState;
-    }
-    axios.get(query).then((result) => {
-      console.log(result.data);
-      setProjects(result.data);
-    });
-  }, [listState]);
 
   return (
     <>
       <NavBar page={"resources"} />
       <div className={classes.container}>
-        <div className={classes.inspoText}></div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-
-            paddingTop: 20,
-          }}
-        >
-          <div className={classes.sideBar}>
-            <div className={classes.sectionOne}>
-              <h1
-                style={{
-                  fontFamily: "Raleway",
-                  color: "#939393",
-                  fontSize: 14,
-                }}
-              >
-                PROJECTS
-              </h1>
-              <button
-                className={classes.button}
-                onClick={() => {
-                  setListState("");
-                }}
-              >
-                Feed
-              </button>
-              <button
-                className={classes.button}
-                onClick={() => {
-                  setListState("");
-                }}
-              >
-                Trending
-              </button>
-              <button
-                className={classes.button}
-                onClick={() => {
-                  setListState("");
-                }}
-              >
-                Search
-              </button>
+        <div className={classes.section2}>
+          <img src={text1} style={{ marginLeft: 200 }} alt="text" />
+        </div>
+        <div className={classes.section}>
+          <p
+            style={{
+              width: 1153,
+              fontFamily: "Raleway",
+              color: "white",
+              fontSize: 28,
+            }}
+          >
+            We are an online community platform that enables female developers
+            of all skill levels to discover, inspire, and grow in a male
+            dominated industry. Through support and mentorship from members of
+            our diverse colony, create and collaborate on projects from across
+            the world to expand your own personal hive.{" "}
+          </p>
+        </div>
+        <div className={classes.section2}>
+          <img src={git} alt="git" style={{ width: 229, padding: 60 }} />
+          <img src={text2} alt="text" />
+        </div>
+        <div className={classes.section3}>
+          <h1 style={{ color: "#FF8776" }}>What is Github?</h1>
+          <p>
+            GitHub is a web-based interface that allows for real-time
+            collaboration. It uses Git (an open source version control
+            software), and lets multiple people make separate changes to web
+            pages at the same time. On Hive, GitHub allows members to work
+            together to build their projects, while permitting mentors to assist
+            along the way.
+          </p>
+          <p>
+            Learn more and sign up{" "}
+            <a style={{ color: "black" }} href="https://github.com/">
+              here.
+            </a>
+          </p>
+        </div>
+        <div className={classes.section4}>
+          <img src={text3} style={{ marginLeft: 90 }} alt="text" />
+          <div
+            style={{
+              marginTop: 50,
+              flexDirection: "row",
+              display: "flex",
+              alignSelf: "center",
+              marginLeft: 80,
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                width: 256,
+                alignItems: "center",
+                justifyContent: "center",
+                padding: 30,
+              }}
+            >
+              <img src={bee} alt="bee" className={classes.logo} />
+              <p className={classes.logoText}>
+                Buzz a post when you see a user’s project you’d like to work on.
+                This will let the user know you are interested.
+              </p>
             </div>
-            <div className={classes.sectionTwo}>
-              <h1
-                style={{
-                  fontFamily: "Raleway",
-                  color: "#939393",
-                  fontSize: 14,
-                }}
-              >
-                SORT BY SKILLS
-              </h1>
-              <button
-                className={classes.button}
-                onClick={() => {
-                  setListState("");
-                }}
-              >
-                {" "}
-                All Projects
-              </button>
-              <button
-                className={classes.button}
-                onClick={() => {
-                  setListState("UI/UX");
-                }}
-              >
-                UI/UX
-              </button>
-              <button
-                className={classes.button}
-                onClick={() => {
-                  setListState("Front-End");
-                }}
-              >
-                Front End
-              </button>
-              <button
-                className={classes.button}
-                onClick={() => {
-                  setListState("Back-End");
-                }}
-              >
-                Back End
-              </button>
-              <button
-                className={classes.button}
-                onClick={() => {
-                  setListState("GameDev/Animation");
-                }}
-              >
-                Game Dev/Animation
-              </button>
-              <button
-                className={classes.button}
-                onClick={() => {
-                  setListState("mentor");
-                }}
-              >
-                Looking for Mentors
-              </button>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                width: 256,
+                alignItems: "center",
+                justifyContent: "center",
+                padding: 30,
+              }}
+            >
+              <img src={eye} alt="eye" className={classes.logo} />
+              <p className={classes.logoText}>
+                View a project you are potentially interested in, or if it just
+                seems appealing. This will not notify the user.
+              </p>
             </div>
-          </div>
-          <div className={classes.items}>
-            {user ? (
-              <Link to="/" className={classes.post}>
-                <h1
-                  style={{
-                    fontSize: 28,
-                    fontFamily: "Raleway",
-                    color: "#656565",
-                    alignSelf: "center",
-                    paddingRight: 8,
-                  }}
-                >
-                  POST PROJECT{" "}
-                </h1>
-                <img src={plus} alt="plus btn" width="38"></img>
-              </Link>
-            ) : null}
-            <div className={classes.cards}>{RenderList(projects)}</div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                width: 256,
+                alignItems: "center",
+                justifyContent: "center",
+                padding: 30,
+              }}
+            >
+              <img src={add} alt="add" className={classes.logo} />
+              <p className={classes.logoText}>
+                Add users to your hive. Keep in touch, work on future projects,
+                share experiences!
+              </p>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                width: 256,
+                alignItems: "center",
+                justifyContent: "center",
+                padding: 30,
+              }}
+            >
+              <img src={star} alt="star" className={classes.logo} />
+              <p className={classes.logoText}>
+                Star your favourite projects. These will appear under the ‘Star’
+                tab of your GitHub account.
+              </p>
+            </div>
           </div>
         </div>
       </div>
