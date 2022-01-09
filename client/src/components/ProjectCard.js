2d799312c2ac2771b34faf4e65bdf5100fe6783d
luxscious <57682940+luxscious@users.noreply.github.com>
2022-01-09 09:38:31 -0500

    added email.js

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -15,6 +15,17 @@ const useStyles = makeStyles((theme) => ({
     width: 760,
   },
 }));
+
+// const handleSubmit = (e) => {
+//   e.preventDefault(); // Prevents default refresh by the browser
+//   emailjs.sendForm(`gmail`, apiKey.TEMPLATE_ID, e.target, apiKey.USER_ID)
+//   .then((result) => {
+//   alert("Message Sent, We will get back to you shortly", result.text);
+//   },
+//   (error) => {
+//   alert("An error occurred, Please try again", error.text);
+//   });
+//   };
 export default function ProjectCard(props) {
   const project = props.project;
   const [cookies, setCookie, removeCookie] = useCookies(["user"]);
